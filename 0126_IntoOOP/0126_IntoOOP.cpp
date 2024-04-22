d5b92d573a57c7db72ff2dba76a34d6a43015b18
Iaannn06 <alfianazad05@gmail.com>
2024-04-22 13:51:57 +0700

    membuat class untuk mata kuliah

--- 0126_IntoOOP/0126_IntoOOP.cpp
+++ 0126_IntoOOP/0126_IntoOOP.cpp
@@ -6,4 +6,16 @@ public:
 	string nama;
 	int umur;
 	string jurusan;
-}
+
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+
+	}
+
+};
+
+class MataKuliah {
+
+};
